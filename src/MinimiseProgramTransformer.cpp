e75fd5e512c2de1a2bce908881190a6cad414ecf
Abdul Zreika <azre6702@uni.sydney.edu.au>
2018-08-08 14:46:38 +1000

    Added variable decomposition class.

--- src/MinimiseProgramTransformer.cpp
+++ src/MinimiseProgramTransformer.cpp
@@ -1,9 +1,85 @@
 #include "AstProgram.h"
 #include "AstTransforms.h"
+#include "AstVisitor.h"
 
 namespace souffle {
+
+class VariableDecomposition {
+private:
+    bool valid;
+    std::vector<std::vector<std::pair<int,int>>> decomposition;
+
+public:
+    VariableDecomposition(const AstClause& clause) {
+        // check validity
+        valid = true;
+
+        // TODO ABDUL get rid of later
+        if (clause.isFact()) {
+            valid = false;
+        }
+
+        // TODO ABDUL add constraints and negtations later
+        for (AstLiteral* lit : clause.getBodyLiterals()) {
+            if (!dynamic_cast<AstAtom*>(lit)) {
+                valid = false;
+                break;
+            }
+        }
+
+        visitDepthFirst(clause, [&](const AstArgument& arg) {
+            if (!dynamic_cast<const AstVariable*>(&arg) && !dynamic_cast<const AstConstant*>(&arg)) {
+                valid = false;
+            }
+        });
+
+        if (valid) {
+            std::map<std::string, std::vector<std::pair<int,int>>> positions;
+
+            visitDepthFirst(clause, [&](const AstVariable& var) {
+                positions[var.getName()] = std::vector<std::pair<int,int>>();
+            });
+
+            AstAtom* head = clause.getHead();
+            std::vector<AstArgument*> headargs = head->getArguments();
+            for (int i = 0; i < headargs.size(); i++) {
+                AstArgument* curr = headargs[i];
+                if (const AstVariable* var = dynamic_cast<const AstVariable*>(curr)) {
+                    positions[var->getName()].push_back(std::make_pair(0,i));
+                } else {
+                    std::cout << "not a var? " << *curr << std::endl;
+                }
+            }
+
+            std::vector<AstLiteral*> bodylits = clause.getBodyLiterals();
+            for (int i = 0; i < bodylits.size(); i++) {
+                const AstAtom* curr = dynamic_cast<const AstAtom*>(bodylits[i]);
+                std::vector<AstArgument*> args = curr->getArguments();
+                for (int j = 0; j < args.size(); j++) {
+                    AstArgument* carg = args[j];
+                    if (const AstVariable* var = dynamic_cast<const AstVariable*>(carg)) {
+                        positions[var->getName()].push_back(std::make_pair(i+1,j));
+                    } else {
+                        std::cout << "not a var? " << *carg << std::endl;
+                    }
+                }
+            }
+
+            std::cout << positions << std::endl;
+        }
+
+        std::cout << clause << " valid? " << valid << std::endl;
+    }
+
+    bool operator==(const VariableDecomposition o) {
+        return false;
+    }
+};
+
 bool areBijectivelyEquivalent(AstClause* left, AstClause* right) {
-    return false;
+    VariableDecomposition leftDec = VariableDecomposition(*left);
+    VariableDecomposition rightDec = VariableDecomposition(*right);
+    return leftDec == rightDec;
 }
 
 bool MinimiseProgramTransformer::transform(AstTranslationUnit& translationUnit) {

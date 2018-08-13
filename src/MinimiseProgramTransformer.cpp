46fec185dbe6d005263385104d6028270a2c5141
Abdul Zreika <azre6702@uni.sydney.edu.au>
2018-08-13 16:12:20 +1000

    Added licenses.
    Luckily, caught the missing license header in the inlining
    transformer file as well before it could get stolen.

--- src/MinimiseProgramTransformer.cpp
+++ src/MinimiseProgramTransformer.cpp
@@ -1,3 +1,19 @@
+/*
+ * Souffle - A Datalog Compiler
+ * Copyright (c) 2018, The Souffle Developers. All rights reserved.
+ * Licensed under the Universal Permissive License v 1.0 as shown at:
+ * - https://opensource.org/licenses/UPL
+ * - <souffle root>/licenses/SOUFFLE-UPL.txt
+ */
+
+/************************************************************************
+ *
+ * @file MinimiseProgramTransformer.cpp
+ *
+ * Define classes and functionality related to program minimisation.
+ *
+ ***********************************************************************/
+
 #include "AstProgram.h"
 #include "AstTransforms.h"
 #include "AstVisitor.h"

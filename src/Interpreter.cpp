bd8b2171c305600be3a1979863f34bce1fa1ea58
Bernhard Scholz <Bernhard.Scholz@sydney.edu.au>
2018-10-11 21:26:56 +1100

    Fixed condition issue

--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -784,8 +784,14 @@ void Interpreter::evalStmt(const RamStatement& stmt) {
         bool visitInsert(const RamInsert& insert) override {
             // run generic query executor
 
-            if(interpreter.evalCond(*insert.getCondition())) {
-               interpreter.evalOp(insert.getOperation());
+	   
+	    const RamCondition *c = insert.getCondition();
+	    if(c != nullptr) { 
+               if(interpreter.evalCond(*insert.getCondition())) {
+                  interpreter.evalOp(insert.getOperation());
+	       }
+	    } else {
+                  interpreter.evalOp(insert.getOperation());
 	    }
             return true;
         }

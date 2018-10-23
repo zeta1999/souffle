234e3c0603dc28b8bdba1ff009ea8b83f5e84a0f
Bernhard Scholz <Bernhard.Scholz@sydney.edu.au>
2018-10-23 16:53:05 +1100

    Fix OS X issue

--- .travis/osx/install_withgcc.sh
+++ .travis/osx/install_withgcc.sh
@@ -10,8 +10,10 @@ rm /usr/local/include/c++
 brew update
 
 # Install gcc instead of gcc-x.x if a current version is preferred
-brew install md5sha1sum bison libtool gcc@7 mcpp
+brew install md5sha1sum bison libtool gcc@7 mcpp libffi
 brew link bison --force
+brew link libffi --force
+export PKG_CONFIG_PATH=/usr/local/opt/libffi/lib/pkgconfig/
 
 # Using 'g++' will call the xcode link to clang
 g++-7 --version

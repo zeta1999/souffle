fa6c3d73dceaf3cd4a2766a9fab8fd531f7eb9d4
Abdul Zreika <azre6702@uni.sydney.edu.au>
2018-10-26 16:48:03 +1100

    Removed fixpoint for empty relation remover.
    RemoveEmptyRelationsTransformer returns true when it shouldn't!!!
    Leading to an infinite loop. Should be fixed at some point.

--- src/main.cpp
+++ src/main.cpp
@@ -458,7 +458,7 @@ int main(int argc, char** argv) {
             std::make_unique<InlineRelationsTransformer>(), std::make_unique<ResolveAliasesTransformer>(),
             std::make_unique<RemoveRedundantRelationsTransformer>(),
             std::make_unique<RemoveRelationCopiesTransformer>(),
-            std::make_unique<FixpointTransformer>(std::make_unique<RemoveEmptyRelationsTransformer>()),
+            std::make_unique<RemoveEmptyRelationsTransformer>(),
             std::make_unique<ReplaceSingletonVariablesTransformer>(),
             std::make_unique<FixpointTransformer>(
                     std::make_unique<PipelineTransformer>(std::make_unique<ReduceExistentialsTransformer>(),

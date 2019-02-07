754a0f45494b1a6134bcf62d57540ddbd0cda085
olligobber <sirolligobbervii@yahoo.com.au>
2019-02-07 16:46:29 +1100

    Added semantic check for typecasts

--- src/AstArgument.h
+++ src/AstArgument.h
@@ -553,7 +553,7 @@ public:
             : value(std::move(value)), type(type) {}
 
     void print(std::ostream& os) const override {
-        os << " as " << type << *value;
+        os << "as " << type << " " << *value;
     }
 
     AstArgument* getValue() const {

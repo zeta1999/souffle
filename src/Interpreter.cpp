b051c2f3f84005d3fddcddc9490ef27e344f8a1d
Bernhard Scholz <Bernhard.Scholz@sydney.edu.au>
2019-04-03 09:12:13 +1100

    Fix RamQuery

--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -847,17 +847,8 @@ void Interpreter::evalStmt(const RamStatement& stmt) {
             return true;
         }
 
-        bool visitQuery(const RamQuery& insert) override {
-            // run generic query executor
-
-            const RamCondition* c = insert.getCondition();
-            if (c != nullptr) {
-                if (interpreter.evalCond(*insert.getCondition())) {
-                    interpreter.evalOp(insert.getOperation());
-                }
-            } else {
-                interpreter.evalOp(insert.getOperation());
-            }
+        bool visitQuery(const RamQuery& query) override {
+            interpreter.evalOp(query.getOperation());
             return true;
         }
 

4cc15bf2e86f8601e242f63e57f62aa8ef049183
Bernhard Scholz <Bernhard.Scholz@sydney.edu.au>
2019-04-24 15:13:47 +1000

    enable indexes again

--- src/main.cpp
+++ src/main.cpp
@@ -482,7 +482,7 @@ int main(int argc, char** argv) {
 
     std::vector<std::unique_ptr<RamTransformer>> ramTransforms;
     ramTransforms.push_back(std::make_unique<HoistConditionsTransformer>());
-    // ramTransforms.push_back(std::make_unique<MakeIndexTransformer>());
+    ramTransforms.push_back(std::make_unique<MakeIndexTransformer>());
     ramTransforms.push_back(std::make_unique<ConvertExistenceChecksTransformer>());
 
     for (const auto& transform : ramTransforms) {

2b46428b3bc48c02668ad1ca92b88dfc6b2cbc42
Bernhard Scholz <Bernhard.Scholz@sydney.edu.au>
2019-04-26 09:05:03 +0930

    Fixed code gen

--- src/Synthesiser.cpp
+++ src/Synthesiser.cpp
@@ -751,10 +751,10 @@ void Synthesiser::emitCode(std::ostream& out, const RamStatement& stmt) {
             auto identifier = aggregate.getIdentifier();
 
             // aggregate tuple storing the result of aggregate
-            std::string tuple_type = "ram::Tuple<RamDomain,1>";
+            std::string tuple_type = "ram::Tuple<RamDomain," + toString(std::max(1u, arity)) + ">";
 
             // declare environment variable
-            out << tuple_type << " env" << identifier << ";\n";
+            out <<"ram::Tuple<RamDomain,1> env" << identifier << ";\n";
 
             // get range to aggregate
             auto keys = keysAnalysis->getRangeQueryColumns(&aggregate);

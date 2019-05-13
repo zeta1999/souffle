c91ce298bdef75098f8c64a26bcb23990251c095
Bernhard Scholz <Bernhard.Scholz@sydney.edu.au>
2019-05-13 18:48:54 +1000

    Extended makeIndexTransformer for IndexScan

--- src/RamTransformer.h
+++ src/RamTransformer.h
@@ -16,7 +16,9 @@
 
 #pragma once
 
+#include <memory>
 #include <string>
+#include <vector>
 
 namespace souffle {
 
@@ -40,5 +42,83 @@ protected:
     /** Transform RAM translation unit */
     virtual bool transform(RamTranslationUnit& translationUnit) = 0;
 };
+
+/**
+ * Composite loop transformer iterating until the encapsulated transformer
+ * does not change the translation unit.
+ */
+
+class RamSequenceTransformer {
+public:
+   RamSequenceTransformer(std::vector<std::unique_ptr<RamTransformer>> tSeq) : transformerSequence(std::move(tSeq)){ }
+   
+   std::string getName() const {
+       return "RamSequenceTransformer";
+   }
+
+   bool transform(RamTranslationUnit& tU) {
+       bool changed = false; 
+       for(auto const &cur: transformerSequence) {
+	       if(cur->apply(tU)) { 
+		       changed = true; 
+	       }
+       }
+       return changed; 
+   }
+
+protected:
+   std::vector<std::unique_ptr<RamTransformer>> transformerSequence; 
+};
+
+/**
+ * Composite loop transformer checking for the change of a transformation.
+ */
+
+class RamIfTransformer {
+public:
+   RamIfTransformer(std::unique_ptr<RamTransformer> tPredicate, std::unique_ptr<RamTransformer> tBody) : 
+	       transformerPredicate(std::move(tPredicate)), transformerBody(std::move(tBody)) { }
+   
+   std::string getName() const {
+       return "RamIfTransformer";
+   }
+
+   bool transform(RamTranslationUnit& tU) {
+       if(transformerPredicate->apply(tU)) {
+          transformerBody->apply(tU);  
+	  return true; 
+       } else {
+	  return false;
+       }
+   }
+
+protected:
+   std::unique_ptr<RamTransformer> transformerPredicate;
+   std::unique_ptr<RamTransformer> transformerBody;
+};
+
+
+/**
+ * Composite sequence transformer that applies sequences of transformations 
+ */
+
+class RamLoopTransformer {
+public:
+   RamLoopTransformer(std::unique_ptr<RamTransformer> tLoop) : transformerLoop(std::move(tLoop)){ }
+
+   std::string getName() const {
+       return "RamLoopTransformer";
+   }
+
+   bool transform(RamTranslationUnit& tU) {
+       while(transformerLoop->apply(tU));
+       return false;
+   }
+
+protected:
+   std::unique_ptr<RamTransformer> transformerLoop;
+};
+
+
 
 }  // end of namespace souffle

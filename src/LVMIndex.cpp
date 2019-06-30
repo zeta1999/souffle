2c50d6e45ca8e4098fa0c0fbc728ad91a95c1bcc
Xiaowen Hu <573119056@qq.com>
2019-06-30 22:39:45 +1000

    Add assertion check in query operation

--- src/LVMIndex.cpp
+++ src/LVMIndex.cpp
@@ -238,6 +238,7 @@ public:
         Entry a = order.encode(low.asTuple<Arity>());
         Entry b = order.encode(high.asTuple<Arity>());
         // Transfer upper_bound to a equivalent lower bound
+        bool fullIndexSearch = true;
         for (size_t i = Arity; i-- > 0;) {
             if (a[i] == MIN_RAM_DOMAIN && b[i] == MAX_RAM_DOMAIN) {
                 b[i] = MIN_RAM_DOMAIN;
@@ -245,9 +246,11 @@ public:
             }
             if (a[i] == b[i]) {
                 b[i] += 1;
+                fullIndexSearch = false;
                 break;
             }
         }
+        assert(fullIndexSearch == false && "Full index search is not allowed in range query\n");
         return std::make_unique<Source>(order, data.lower_bound(a), data.lower_bound(b));
     }
 

4cd337c3629f36f057aabdaa85f870e126870747
lyndonhenry <lyndonhenrydotnet@gmail.com>
2020-01-21 15:08:03 +1100

    Fix clang-format version in pre-commit script

--- utilities/git/hooks/pre-commit.sh
+++ utilities/git/hooks/pre-commit.sh
@@ -2,17 +2,23 @@
 
 set -oue pipefail
 
-if [ $(which clang-format-7) ]
+if [ ! $(which clang-format) ]
 then
-    clang-format-7 \
-            -i \
-            -style=file \
-            src/*.h \
-            src/*.cpp \
-            src/test/*.h \
-            src/test/*.cpp \
-            tests/interface/*/*.cpp
-else
-    echo "Program 'clang-format-7' not found!"
+    echo "Error: program 'clang-format' not found!"
     exit 1
 fi
+
+if [ "$(clang-format --version | sed 's/.*version //;s/\..*//')" -lt "7" ]
+then
+   echo "Error: program 'clang-format' must be version 7 or later!"
+   exit 1
+fi
+
+clang-format \
+        -i \
+        -style=file \
+        src/*.h \
+        src/*.cpp \
+        src/test/*.h \
+        src/test/*.cpp \
+        tests/interface/*/*.cpp

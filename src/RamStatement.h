a57afdfd270eda75617dc9f706cadc267fe946c0
yuli6313 <yuli6313@uni.sydney.edu.au>
2020-01-21 16:24:44 +1100

    fix clone() for RamMerge

--- src/RamStatement.h
+++ src/RamStatement.h
@@ -298,8 +298,8 @@ public:
     }
 
     RamMerge* clone() const override {
-        auto* res = new RamMerge(std::unique_ptr<RamRelationReference>(first->clone()),
-                std::unique_ptr<RamRelationReference>(second->clone()));
+        auto* res = new RamMerge(std::unique_ptr<RamRelationReference>(second->clone()),
+                std::unique_ptr<RamRelationReference>(first->clone()));
         return res;
     }
 

6e67d2a77fe5eceede716e05f41430aab2957360
Tytus Metrycki <tytus.metrycki@gmail.com>
2020-02-07 13:51:12 +1100

    Remove record table from InterpreterGenerator

--- src/InterpreterGenerator.h
+++ src/InterpreterGenerator.h
@@ -22,7 +22,6 @@
 #include "InterpreterPreamble.h"
 #include "RamIndexAnalysis.h"
 #include "RamVisitor.h"
-#include "RecordTable.h"
 #include <cassert>
 #include <memory>
 #include <queue>
@@ -275,7 +274,6 @@ public:
         NodePtrVec children;
         children.push_back(visit(lookup.getExpression()));
         children.push_back(visitTupleOperation(lookup));
-        RecordTable::createRecordMap(lookup.getArity());
         return std::make_unique<InterpreterNode>(I_UnpackRecord, &lookup, std::move(children));
     }
 

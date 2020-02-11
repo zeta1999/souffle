f0eb7da86738e4cc29d182a559501e6a11ac5a24
Tytus Metrycki <tytus.metrycki@gmail.com>
2020-02-11 17:17:13 +1100

    More unit tests

--- src/test/record_table_test.cpp
+++ src/test/record_table_test.cpp
@@ -17,8 +17,12 @@
 #include "CompiledTuple.h"
 #include "RecordTable.h"
 #include "test.h"
+#include <random>
+#include <vector>
 
 namespace souffle::test {
+
+#define NUMBER_OF_TESTS 100
 
 TEST(Pack, Tuple) {
     RecordTable recordTable;
@@ -33,16 +37,39 @@ TEST(Pack, Tuple) {
     }
 }
 
+// Generate random tuples
+// pack them all
+// unpack and test for equality
 TEST(PackUnpack, Tuple) {
+    constexpr size_t tupleSize = 3;
+    using tupleType = ram::Tuple<RamDomain, tupleSize>;
+
     RecordTable recordTable;
 
-    const ram::Tuple<RamDomain, 3> toPack = {{1, 2, 3}};
+    // Setup random number generation
+    std::default_random_engine randomGenerator(3);
+    std::uniform_int_distribution<RamDomain> distribution(
+            std::numeric_limits<RamDomain>::lowest(), std::numeric_limits<RamDomain>::max());
 
-    RamDomain ref = recordTable.pack(toPack);
+    auto random = std::bind(distribution, randomGenerator);
 
-    const ram::Tuple<RamDomain, 3> unPacked = recordTable.unpackTuple<RamDomain, 3>(ref);
+    // Tuples that will be packed
+    std::vector<tupleType> toPack(NUMBER_OF_TESTS);
 
-    EXPECT_EQ(toPack, unPacked);
+    // Tuple reference after they are packed.
+    std::vector<RamDomain> tupleRef(NUMBER_OF_TESTS);
+
+    // Generate and pack the tuples
+    for (size_t i = 0; i < NUMBER_OF_TESTS; ++i) {
+        toPack[i] = {{random(), random(), random()}};
+        tupleRef[i] = recordTable.pack(toPack[i]);
+    }
+
+    // unpack and test
+    for (size_t i = 0; i < NUMBER_OF_TESTS; ++i) {
+        auto unpacked = recordTable.unpackTuple<RamDomain, tupleSize>(tupleRef[i]);
+        EXPECT_EQ(toPack[i], unpacked);
+    }
 }
 
 }  // namespace souffle::test

a959ceb805b932e23fb0e010fa9437a938fd0ec2
GaloisNeko <11177488+GaloisNeko@users.noreply.github.com>
2020-06-21 22:31:54 +1000

    WIP

--- src/WriterStreamJSON.h
+++ src/WriterStreamJSON.h
@@ -23,6 +23,9 @@
 #include <map>
 #include <ostream>
 #include <string>
+#include <vector>
+#include <variant>
+#include <stack>
 
 namespace souffle {
 
@@ -36,18 +39,26 @@ protected:
     const bool beautify;
 
     void writeNextTupleJSON(std::ostream& destination, const RamDomain* tuple) {
-        writeNextTupleElement(destination, typeAttributes.at(0), tuple[0]);
+        std::vector<Json> result;
 
-        for (size_t col = 1; col < arity; ++col) {
+        bool isFirst = true;
+        destination << "[";
+        for (size_t col = 0; col < arity; ++col) {
             writeNextTupleElement(destination, typeAttributes.at(col), tuple[col]);
+            if (isFirst) {
+                isFirst = false;
+            } else {
+                destination << ",\n";
+            }
         }
 
-        destination << "\n";
+        // Output a JSON array for all tuples
+        destination << "]";
     }
 
-    void writeNextTupleElement(std::ostream& destination, const std::string& type, RamDomain value) {
+    Json writeNextTupleElement(std::ostream& destination, const std::string& type, RamDomain value) {
         switch (type[0]) {
-            case 's': destination << "\"" << symbolTable.unsafeResolve(value) << "\""; break;
+            case 's': destination << symbolTable.unsafeResolve(value); break;
             case 'i': destination << value; break;
             case 'u': destination << ramBitCast<RamUnsigned>(value); break;
             case 'f': destination << ramBitCast<RamFloat>(value); break;
@@ -57,45 +68,30 @@ protected:
     }
 
     void outputJSONRecord(std::ostream& destination, const RamDomain value, const std::string& name) {
-        auto&& recordInfo = types["records"][name];
+        using ValueTuple = std::pair<const RamDomain, const std::string&>;
+        std::stack<std::variant<ValueTuple, std::string>> worklist;
+        worklist.push(std::make_pair(value, name));
 
-        // Check if record type information are present
-        assert(!recordInfo.is_null() && "Missing record type information");
+        while(!worklist.empty()) {
+            auto curr = std::move(worklist.top());
+            worklist.pop();
 
-        // Check for nil and return empty JSON object
-        if (value == 0) {
-            destination << "{}";
-            return;
-        }
-
-        Json recordJson = Json::object {};
-        auto&& recordTypes = recordInfo["types"];
-        const size_t recordArity = recordInfo["arity"].long_value();
-
-        const RamDomain* tuplePtr = recordTable.unpack(value, recordArity);
-
-        destination << "[";
-
-        // print record's elements
-        for (size_t i = 0; i < recordArity; ++i) {
-            if (i > 0) {
-                destination << ", ";
+            if (std::holds_alternative<std::string>(curr)) {
+                destination << std::get<std::string>(curr);
+                continue;
             }
 
-            const std::string& recordType = recordTypes[i].string_value();
-            const RamDomain recordValue = tuplePtr[i];
-
-            switch (recordType[0]) {
-                case 'i': destination << recordValue; break;
-                case 'f': destination << ramBitCast<RamFloat>(recordValue); break;
-                case 'u': destination << ramBitCast<RamUnsigned>(recordValue); break;
-                case 's': destination << symbolTable.unsafeResolve(recordValue); break;
-                case 'r': outputRecord(destination, recordValue, recordType); break;
-                default: fatal("Unsupported type attribute: `%c`", recordType[0]);
+            const std::string& type = std::get<ValueTuple>(curr).second;
+            RamDomain value = std::get<ValueTuple>(curr).first;
+            switch (type[0]) {
+                case 's': return Json(symbolTable.unsafeResolve(value)); break;
+                case 'i': return Json((int) value); break;
+                case 'u': return Json((int) ramBitCast<RamUnsigned>(value)); break;
+                case 'f': return Json(ramBitCast<RamFloat>(value)); break;
+                case 'r': return outputJSONRecord(value, type); break;
+                default: fatal("unsupported type attribute: `%c`", type[0]);
             }
         }
-        destination << "]";
-
     }
 
 };

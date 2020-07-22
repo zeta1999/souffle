8c772862a1c3d34b1cb28607ac6974d638a70f8b
David Zhao <taipan.snake13@gmail.com>
2020-07-22 18:49:41 +1000

    fix issues synthesising values for ProvenanceExistenceChecks

--- src/AstTranslator.cpp
+++ src/AstTranslator.cpp
@@ -295,7 +295,7 @@ std::unique_ptr<RamCondition> AstTranslator::translateConstraint(
         std::unique_ptr<RamCondition> visitProvenanceNegation(const AstProvenanceNegation& neg) override {
             const auto* atom = neg.getAtom();
             size_t auxiliaryArity = translator.getEvaluationArity(atom);
-            assert(auxiliaryArity < atom->getArity() && "auxiliary arity out of bounds");
+            assert(auxiliaryArity <= atom->getArity() && "auxiliary arity out of bounds");
             size_t arity = atom->getArity() - auxiliaryArity;
             std::vector<std::unique_ptr<RamExpression>> values;
 
